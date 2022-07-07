3d2f910f13e6401deea5362c56e5192df0168464
rootjk <2310738522@qq.com>
2022-07-07 15:16:47 +0800

    完成MP分页查询

--- src/main/java/com/itjk/controller/UserController.java
+++ src/main/java/com/itjk/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.itjk.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itjk.dao.UserDao;
 import com.itjk.dao.UserDao1;
 import com.itjk.domain.User;
 import com.itjk.service.UserService;
+import com.itjk.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +20,8 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserDao userDao;
-   /* @Autowired
-    private UserService userService;*/
+    @Autowired
+    private UserServiceImpl userServiceimpl;
 
     @Autowired
     private UserDao1 userDao1;
@@ -58,17 +60,25 @@ public class UserController {
         return new Result(null,UserList,null);
     }
 
-    //分页查询
+    //分页查询 mybatis-
     @CrossOrigin
     @GetMapping("/page")
-    public  Map<String,Object> GetByPage(@RequestParam Integer pageNum,@RequestParam Integer pageSize){
-        pageNum =(pageNum - 1) * pageSize;
-        List<User> data = userDao.selectPage(pageNum, pageSize);
-        Integer total = userDao.selectPageTotal();
-        Map<String,Object> res = new HashMap<>();
-        res.put("data",data);
-        res.put("total",total);
-        return res;
+    public IPage<User> GetByPage(@RequestParam Integer pageNum,
+                                 @RequestParam Integer pageSize,
+                                 @RequestParam(defaultValue = "") String username,
+                                 @RequestParam(defaultValue = "") String nickname,
+                                 @RequestParam(defaultValue = "") String email
+                                 ){
+        IPage<User> page = new Page<>(pageNum,pageSize);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        System.out.println("被执行");
+        queryWrapper.like("username",username);//模糊查询
+        queryWrapper.like("username",nickname);
+        queryWrapper.like("username",email);
+
+        //queryWrapper.like(Strings.isNotEmpty(username),"username",username);
+        return userServiceimpl.page(page,queryWrapper);
+
     }
 
     /*@GetMapping
--- src/main/java/com/itjk/dao/UserDao1.java
+++ src/main/java/com/itjk/dao/UserDao1.java
@@ -1,9 +1,12 @@
 package com.itjk.dao;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.itjk.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserDao1 extends BaseMapper<User> {
+    void page(IPage<User> page, QueryWrapper<User> queryWrapper);
 }
